64d76e6d7745de07c7486e59213324af61e6d3ad
Gabriel Gotlib <gagotlib@gmail.com>
2024-08-16 11:12:08 +0200

    improving the catalog, sorting and filter interaction

--- src/components/SortSelector.jsx
+++ src/components/SortSelector.jsx
@@ -1,52 +1,92 @@
 import React, { useState } from 'react'
 import { View, Text, StyleSheet, Pressable } from 'react-native'
-import { Picker } from '@react-native-picker/picker'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+// import { Picker } from '@react-native-picker/picker'
 
-export default function App({ selectedValue, setSelectedValue }) {
-	const [isSortingOpen, setIsSortingOpen] = useState(false)
-
+export default function App({ selectedValue, setSelectedValue, setIsFilterOpen, isSortingOpen, setIsSortingOpen, setIsGenreOpen, setIsDirectorOpen, setIsYearOpen }) {
 	return (
-		<View style={styles.container}>
-			<Pressable>
-				<Text> Sort by .. </Text>
-				{/* <Text style={styles.label}>Select an option:</Text>
-			<Picker selectedValue={selectedValue} style={styles.picker} onValueChange={(itemValue) => setSelectedValue(itemValue)}>
-				<Picker.Item label='Sort by...' value={null} />
-				<Picker.Item label='Title (A-Z)' value='Title' />
-				<Picker.Item label='Year (earliest - latest)' value='Newest' />
-				<Picker.Item label='Year (latest - earliest)' value='Latest' />
-
-			</Picker>
-			{selectedValue && <Text style={styles.selectedValue}>Selected: {selectedValue}</Text>} */}
+		<View className='flex w-1/2 h-auto rounded-lg item-center justify-center '>
+			<Pressable
+				onPress={() => {
+					setIsSortingOpen(!isSortingOpen)
+					setIsFilterOpen(false)
+					setIsGenreOpen(false)
+					setIsDirectorOpen(false)
+					setIsYearOpen(false)
+				}}
+				className='  h-10 rounded-lg items-center justify-center '
+			>
+				<View style={styles.container}>
+					<Text style={styles.label}>{!selectedValue ?  <FontAwesome name='sort-amount-desc' size={20} color='black' /> : selectedValue} </Text>
+				</View>
 			</Pressable>
+			{isSortingOpen && (
+				<View>
+					<Pressable
+						onPress={() => {
+							setSelectedValue('Title')
+							setIsSortingOpen(false)
+						}}
+						style={styles.picker}
+					>
+						<Text style={styles.selectedValue}>Title (A-Z)</Text>
+					</Pressable>
+					<Pressable
+						onPress={() => {
+							setSelectedValue('Newest')
+							setIsSortingOpen(false)
+						}}
+						style={styles.picker}
+					>
+						<Text style={styles.selectedValue}>Year (Newest - Oldest)</Text>
+					</Pressable>
+					<Pressable
+						onPress={() => {
+							setSelectedValue('Oldest')
+							setIsSortingOpen(false)
+						}}
+						style={styles.picker}
+					>
+						<Text style={styles.selectedValue}>Year (Oldest - Newest)</Text>
+					</Pressable>
+					<Pressable
+						onPress={() => {
+							setSelectedValue(null)
+							setIsSortingOpen(false)
+						}}
+						style={styles.picker}
+					>
+						<Text style={styles.selectedValue}>Cancel</Text>
+					</Pressable>
+				</View>
+			)}
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
 	container: {
-		// display: 'flex',
-		// height: 10,
-		// justifyContent: 'center',
-		// paddingHorizontal: 20,
-		// marginTop: 20,
-		// height: 10,
-		borderColor: 'black',
-		borderWidth: 1,
-		height: 40,
+		// height: '100%',
 		justifyContent: 'center',
+		alignItems: 'center',
 		width: '100%',
+		marginLeft: 5
 	},
 	label: {
-		fontSize: 16,
-		marginBottom: 10
+		fontSize: 14,
+		fontWeight: 'bold',
 	},
 	picker: {
 		height: 50,
-		width: '100%'
+		width: '100%',
+		// borderWidth: 2,
+		// borderColor: 'black',
+		// borderRadius: 5,
+		alignItems: 'center',
+		justifyContent: 'center'
 	},
 	selectedValue: {
-		marginTop: 10,
-		fontSize: 20
+		marginLeft: 5,
+		fontSize: 14
 	}
 })
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,8 +1,9 @@
-import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import useMovies from '../hooks/useMovies'
 import MovieCard from '../components/MovieCard'
 import { useEffect, useState } from 'react'
 import SortSelector from '../components/SortSelector'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
 
 export default function Catalog() {
 	const { movies = [] } = useMovies() // Custom hook that fetches movies
@@ -12,7 +13,12 @@ export default function Catalog() {
 	const [selectedValue, setSelectedValue] = useState(null)
 	const [isFilterOpen, setIsFilterOpen] = useState(false)
 	const [isGenreOpen, setIsGenreOpen] = useState(false)
-	const [selectedGenres, setSelectedGenres] = useState([]) // Array for multiple genre selection
+	const [selectedGenres, setSelectedGenres] = useState([])
+	const [isDirectorOpen, setIsDirectorOpen] = useState(false)
+	const [isYearOpen, setIsYearOpen] = useState(false)
+	const [selectedDirectors, setSelectedDirectors] = useState([]) // Array for multiple director selection
+	const [selectedYears, setSelectedYears] = useState([]) // Array for multiple year selection
+	const [isSortingOpen, setIsSortingOpen] = useState(false)
 
 	useEffect(() => {
 		if (!Array.isArray(movies)) return
@@ -22,54 +28,74 @@ export default function Catalog() {
 			sortedArray.sort((a, b) => a.title.localeCompare(b.title))
 		} else if (selectedValue === 'Newest') {
 			sortedArray.sort((a, b) => b.year - a.year)
-		} else if (selectedValue === 'Latest') {
+		} else if (selectedValue === 'Oldest') {
 			sortedArray.sort((a, b) => a.year - b.year)
 		}
 
 		setSortedMovies(sortedArray)
 	}, [selectedValue, movies])
 
-	const toggleGenreSelection = (genre) => {
-		if (selectedGenres.includes(genre)) {
-			setSelectedGenres(selectedGenres.filter((g) => g !== genre))
+	const toggleSelection = (item, selectedArray, setSelectedArray) => {
+		if (selectedArray.includes(item)) {
+			setSelectedArray(selectedArray.filter((i) => i !== item))
 		} else {
-			setSelectedGenres([...selectedGenres, genre])
+			setSelectedArray([...selectedArray, item])
 		}
 	}
 
 	const filteredMovies = sortedMovies?.filter((movie) => {
 		const matchesSearchTerm = movie.title.toLowerCase().includes(searchTerm.toLowerCase()) || movie.director.toLowerCase().includes(searchTerm.toLowerCase()) || movie.year === Number(searchTerm)
-
 		const matchesGenres = selectedGenres.length === 0 || selectedGenres.every((selectedGenre) => movie.genre.map((g) => g.toLowerCase()).includes(selectedGenre))
+		const matchesDirectors = selectedDirectors.length === 0 || selectedDirectors.includes(movie.director.toLowerCase())
 
-		return matchesSearchTerm && matchesGenres
+		const matchesYears = selectedYears.length === 0 || selectedYears.includes(movie.year)
+
+		return matchesSearchTerm && matchesGenres && matchesDirectors && matchesYears
 	})
 
-	const genres = ['Action', 'Biography', 'Sci-Fi', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Crime', 'Mystery', 'Thriller', 'Western', 'Romance']
+	const movieGenres = [...new Set(movies?.flatMap((movie) => movie.genre))]
+	const movieYears = [...new Set(movies?.map((movie) => movie.year))]
+	const movieDirectors = [...new Set(movies?.map((movie) => movie.director))]
 
 	return (
-		<View className='px-2 mt-14'>
+		<View className='px-0 mt-10'>
 			<View className='flex'>
-				<View className='flex flex-row '>
+				<View className='flex flex-row px-2 '>
 					<Pressable
 						onPress={() => {
 							setIsFilterOpen(!isFilterOpen)
 							setIsGenreOpen(false)
+							setIsDirectorOpen(false)
+							setIsYearOpen(false)
+							setIsSortingOpen(false)
 						}}
-						className='items-center w-1/2 px-4 py-2 border-2 border-blue-500 rounded-lg '
+						className='items-center w-1/2 justify-center  rounded-lg h-10 '
 					>
-						<Text className=''>Filters..</Text>
+						<Text className='font-bold '>
+							 <FontAwesome name='filter' size={24} color='black' />
+						</Text>
 					</Pressable>
-					<SortSelector selectedValue={selectedValue} setSelectedValue={setSelectedValue} />
+					<SortSelector
+						selectedValue={selectedValue}
+						setSelectedValue={setSelectedValue}
+						setIsFilterOpen={setIsFilterOpen}
+						isSortingOpen={isSortingOpen}
+						setIsSortingOpen={setIsSortingOpen}
+						setIsDirectorOpen={setIsDirectorOpen}
+						setIsGenreOpen={setIsGenreOpen}
+						setIsYearOpen={setIsYearOpen}
+					/>
 				</View>
 
-				<View className='my-4 '>
+				<View className='my-1 '>
 					{isFilterOpen && (
-						<View>
+						<View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
 							<Pressable
 								onPress={() => {
 									setSearchTerm('')
 									setSelectedGenres([])
+									setSelectedDirectors([])
+									setSelectedYears([])
 								}}
 								className='items-center w-1/2 px-4 py-2 mb-2 border-2 border-red-500 rounded-lg'
 							>
@@ -79,22 +105,44 @@ export default function Catalog() {
 								className='items-center w-1/2 px-4 py-2 mb-2 border-2 border-blue-500 rounded-lg'
 								onPress={() => {
 									setIsGenreOpen(!isGenreOpen)
+									setIsDirectorOpen(false)
+									setIsYearOpen(false)
 								}}
 							>
-								<Text>Filter by genre</Text>
+								<Text>Filter by Genre</Text>
+							</Pressable>
+							<Pressable
+								className='items-center w-1/2 px-4 py-2 mb-2 border-2 border-blue-500 rounded-lg'
+								onPress={() => {
+									setIsDirectorOpen(!isDirectorOpen)
+									setIsGenreOpen(false)
+									setIsYearOpen(false)
+								}}
+							>
+								<Text>Filter by Director</Text>
+							</Pressable>
+							<Pressable
+								className='items-center w-1/2 px-4 py-2 mb-2 border-2 border-blue-500 rounded-lg'
+								onPress={() => {
+									setIsYearOpen(!isYearOpen)
+									setIsGenreOpen(false)
+									setIsDirectorOpen(false)
+								}}
+							>
+								<Text>Filter by Year</Text>
 							</Pressable>
 						</View>
 					)}
 
 					{isGenreOpen && (
 						<FlatList
-							data={genres}
+							data={movieGenres}
 							renderItem={({ item: genre }) => (
 								<Pressable
 									className={`flex items-center justify-center w-1/3 px-4 py-2 m-2 my-2 border-2 rounded-lg ${
 										selectedGenres.includes(genre.toLowerCase()) ? 'border-green-500 bg-green-100' : 'border-blue-500'
 									}`}
-									onPress={() => toggleGenreSelection(genre.toLowerCase())}
+									onPress={() => toggleSelection(genre.toLowerCase(), selectedGenres, setSelectedGenres)}
 								>
 									<Text>{genre}</Text>
 								</Pressable>
@@ -104,10 +152,50 @@ export default function Catalog() {
 							columnWrapperStyle={{ justifyContent: 'space-between' }}
 						/>
 					)}
+
+					{isDirectorOpen && (
+						<FlatList
+							data={movieDirectors}
+							renderItem={({ item: director }) => (
+								<Pressable
+									className={`flex items-center justify-center w-5/12 px-4 py-2  my-2 border-2 rounded-lg ${
+										selectedDirectors.includes(director.toLowerCase()) ? 'border-green-500 bg-green-100' : 'border-blue-500'
+									}`}
+									onPress={() => toggleSelection(director.toLowerCase(), selectedDirectors, setSelectedDirectors)}
+								>
+									<Text>{director}</Text>
+								</Pressable>
+							)}
+							keyExtractor={(item) => item}
+							numColumns={2}
+							columnWrapperStyle={{ justifyContent: 'space-around' }}
+						/>
+					)}
+
+					{isYearOpen && (
+						<FlatList
+							data={movieYears}
+							renderItem={({ item: year }) => (
+								<Pressable
+									className={`flex items-center justify-center w-1/3 px-4 py-2 m-2 my-2 border-2 rounded-lg ${selectedYears.includes(year) ? 'border-green-500 bg-green-100' : 'border-blue-500'}`}
+									onPress={() => toggleSelection(year, selectedYears, setSelectedYears)}
+								>
+									<Text>{year}</Text>
+								</Pressable>
+							)}
+							keyExtractor={(item) => item.toString()}
+							numColumns={2}
+							columnWrapperStyle={{ justifyContent: 'space-between' }}
+						/>
+					)}
 				</View>
 			</View>
-			<View className='mb-40'>
-				{filteredMovies.length > 0 ? <FlatList data={filteredMovies} renderItem={({ item: movie }) => <MovieCard movie={movie} />} /> : <Text style={styles.text}>There are no movies</Text>}
+			<View className='pb-24'>
+				{filteredMovies.length > 0 ? (
+					<FlatList data={filteredMovies} renderItem={({ item: movie }) => <MovieCard movie={movie} />} />
+				) : (
+					<Text style={styles.text}>There are no movies matching your search</Text>
+				)}
 			</View>
 		</View>
 	)
